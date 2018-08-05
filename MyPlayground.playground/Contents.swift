4f6f45d90d00a4e370d2c80e3e090bc60c5b5586
Aoyagi Naoto <aoyaginaoto@vertexnoMacBook-ea-7.local>
2018-08-05 10:50:01 +0900

    First commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import function
+import UIKit
+
+let nameLavel:UILavel = UILavel()
+namelavel.text
+
+class iPhone{
+    
+    var width:Double = 360
+    var height:Double = 420
+    var color:String = "Gold"
+    var obVersion:Double = 11.1
+
+    var version:String = "iPhoneX"
+    
+    func makeiPhone(
+}
